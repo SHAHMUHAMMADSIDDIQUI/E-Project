0cce2b42e9aa7a60a1b153cfde67519bf8850f17
SHAHMUHAMMADSIDDIQUI <shahmuhammadsiddiqui420@gmail.com>
2024-03-19 20:31:00 +0500

    All Files Added

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+/* // Simulate continuous scrolling ticker
+function updateTicker() {
+  const date = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString();
+  document.getElementById('ticker').textContent = `Date: ${date} | Time: ${time}`;
+}
+
+// Update ticker and visitor count every second
+setInterval(() => {
+  updateTicker();
+  updateVisitorCount();
+}, 1000);
+// Initialize ticker
+updateTicker();
+
+ */
+// Retrieve visitor count from localStorage or initialize it to 0
+let visitorCount = localStorage.getItem('visitorCount');
+visitorCount = visitorCount ? parseInt(visitorCount) : 0;
+
+// Update visitor count display
+document.getElementById('visitor-count').textContent = visitorCount;
+
+// Increment visitor count when page is loaded
+visitorCount++;
+// Save updated visitor count to localStorage
+localStorage.setItem('visitorCount', visitorCount.toString());
+
+
+
+// Define a function to update the ticker and visitor count
+function updateTickerAndVisitorCount() {
+    const date = new Date().toLocaleDateString();
+    const time = new Date().toLocaleTimeString();
+    document.getElementById('ticker').textContent = `Date: ${date} | Time: ${time}`;
+  }
+  
+  // Call the function to initialize the ticker and visitor count
+  updateTickerAndVisitorCount();
+  
+  // Update ticker and visitor count every second
+  setInterval(updateTickerAndVisitorCount, 1000);
