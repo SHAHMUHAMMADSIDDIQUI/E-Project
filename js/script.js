bbfeda2a3da49115ba816ef06feee22724af3fc9
SHAHMUHAMMADSIDDIQUI <shahmuhammadsiddiqui420@gmail.com>
2024-03-20 14:51:27 +0500

    deal bar added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,4 @@
-
-/* // Simulate continuous scrolling ticker
-function updateTicker() {
-  const date = new Date().toLocaleDateString();
-  const time = new Date().toLocaleTimeString();
-  document.getElementById('ticker').textContent = `Date: ${date} | Time: ${time}`;
-}
-
-// Update ticker and visitor count every second
-setInterval(() => {
-  updateTicker();
-  updateVisitorCount();
-}, 1000);
-// Initialize ticker
-updateTicker();
-
- */
+/*[[[[[    Visitior Count   ]]]]]]]]*/
 // Retrieve visitor count from localStorage or initialize it to 0
 let visitorCount = localStorage.getItem('visitorCount');
 visitorCount = visitorCount ? parseInt(visitorCount) : 0;
@@ -26,18 +10,26 @@ document.getElementById('visitor-count').textContent = visitorCount;
 visitorCount++;
 // Save updated visitor count to localStorage
 localStorage.setItem('visitorCount', visitorCount.toString());
+// Call the function to initialize the ticker and visitor count
+updateTickerAndVisitorCount();
+
+// Update ticker and visitor count every second
+setInterval(updateTickerAndVisitorCount, 1000);
 
 
 
+/*[[[[[[[    Date And Time   ]]]]]]]]*/
 // Define a function to update the ticker and visitor count
 function updateTickerAndVisitorCount() {
-    const date = new Date().toLocaleDateString();
-    const time = new Date().toLocaleTimeString();
-    document.getElementById('ticker').textContent = `Date: ${date} | Time: ${time}`;
-  }
-  
-  // Call the function to initialize the ticker and visitor count
-  updateTickerAndVisitorCount();
-  
-  // Update ticker and visitor count every second
-  setInterval(updateTickerAndVisitorCount, 1000);
+  const date = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString();
+  document.getElementById('ticker').textContent = `Date: ${date} | Time: ${time}`;
+}
+
+
+/* $(document).ready(function(){
+  $(".text").each(function(index) {
+      $(this).delay(3000*index).animate({opacity: 1}, 1000);
+  });
+}); */
+
